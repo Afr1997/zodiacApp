fdaf7872f1d9bcd1feb1e357373fef284754c0bb
Afr1997 <afreenahmed9@outlook.com>
2021-08-19 18:18:51 -0400

    Few Changes Made

---
+++ client/js/scripts.js
@@ -0,0 +1,68 @@
+var zodiacs=[
+    {
+        name:"Aries",
+        month:"March 21-April 20",
+        decription:"Aries, as the first sign of the zodiac, you’re the eldest child in this family. You set the pace while the rest of the world plays catch-up. The sheer force of your determination and drive gets you where you need to go, and fast. Simply put, you’re a warrior. It makes sense, then, that Aries is ruled by Mars, the planet of action and, according to ancient Roman mythology, the God of War. Mars is all about energy, passion and self-starting, leaving Aries with an inherent, undeniable confidence. Not only do you know you’ll get the job done, but you’re sure there’s nobody better suited for it. That’s Mars talking.",
+
+    },
+    {
+        name:"Taurus",
+        month:"April 20 to May 20",
+        description:"When you think of Venus, odds are your mind drifts to love, beauty or women’s tennis. If you’re a Taurus, the first two (and maybe the third, but that’s more of a personal preference) are particularly relevant to you. Lover of good food, sensuous fabrics, fine art and, well, love, you esteem all things aesthetically pleasing. Venus’ influence is strong in the sign of the bull, encouraging Taureans to pamper their senses. The Goddess of Love also plays a part in your deep loyalty and commitment to those you care about. She doesn’t mess around when it comes to her heart, and neither do you",
+
+    },
+    {
+        name:"Gemini",
+        month:"May 21 to June 20",
+        description:"Ruling Planet: Mercury Gemini, you’re ruled by Mercury, the planet of communication. Surprised? Didn’t think so. In Roman mythology, Mercury, thought to be the messenger of the gods, delivered news and shared opinions. The planet encourages Geminis to do the same, hence their verbose nature. All three air signs (Gemini, Libra and Aquarius) are intellectually driven, but Mercury’s emphasis on the mind encourages Geminis in particular to learn, think and share more.",
+
+        
+    },
+    {
+        name:"Cancer",
+        month:"June 21 to July 22",
+        description:" Ruling Planet: The Moon  Cancer, your feelings change as quickly as the tides. Perhaps this is a result of being ruled by the moon, the planet responsible for the ebbs and flows of human emotion. The moon influences how we feel, and Cancer, you feel…a lot. You can be more sensitive than the other signs and more in touch with your inner depths. Your moods may change quickly but you’re always sure of what they are. The moon also represents the celestial “mother” figure, influencing Cancers to care deeply about the ties of home and family and develop a penchant for care-taking. You’ll soon realize that, because of the moon’s influence, you seek from others exactly what you provide: safety and comfort.",
+
+    },
+    {
+        name:"Leo",
+        month:"July 23 to Aug 22",
+        description:"The sun rules you, Leo. The brightest star for the brightest sign, it makes sense that your planetary ruler requires the occasional pair of shades. So do you. Your exuberance, your kindness, your seemingly never-ending supply of energy — it’s all thanks to the sun’s influence. The sun represents the self, the core principles of our being that make up who we are, so it makes sense that you’re the most self-involved sign. While that may sound negative, it’s actually a strength. You’re in touch with yourself, proud of who you are and not afraid to sing it from the rooftops. ",
+    },
+    {
+        name:"Virgo",
+        month:"Aug 23 to Sept 22",
+        description:"You may have noticed that there are more signs than there are planets. Born to problem-solve, you Virgos are skilled sharers, and your ruling planet is no exception. You share your ruling planet, Mercury, with Gemini. Maybe not as well known as Gemini’s realm of communication, Mercury also handles logic, reasoning and productivity (admit it, Virgo, your three favorite things). Mercury’s influence over Virgo is expressed through an innate ability to discern and discriminate, making your sign a natural born critic, in the good sense. You’re hyper-focused on sifting through the noise of the world in an organized, precise manner that leaves little to the imagination."
+    },
+    {
+        name:"Libra",
+        month:"Sept 22 to Oct 22",
+        description:"Another diplomatic sharer, you share your ruling planet, Venus, with Taurus. For you, Libra, Venus comes through in your deep commitment to balance and partnership. Venus’ influence makes you value harmony in both yourself and your relationships. You’re a natural mediator; you appreciate fairness and honesty while working hard to protect those you love. Your relationships mean a lot to you. You take pride in being a good friend and partner and look for those qualities in others. "
+    },
+    {
+        name:"Scorpio",
+        month:"oct 23 to Nov 21",
+        description:"Scorpio, you have a dark side. Maybe this is because you’re ruled by Pluto, the small but mighty planet orbiting farthest from the sun. Pluto is associated with darkness, the subconscious, death and rebirth. Not exactly lighthearted stuff. But you don’t like when things are lighthearted; you crave mystery and intensity. Pluto plays a part in your never-ending fascination with all things hush-hush and underground. This ability to get in touch with your unconscious self makes you one of the most powerful signs in the zodiac. "
+    },
+    {
+        name:"Saggitarius",
+        month:"Nov 22 to Dec 21",
+        description: " A runner, thinker and doer, you’re always on the move. The last fire sign, you’re ruled by Jupiter, the planet of luck, good fortune and exploration (of both knowledge and spirituality). It’s definitely a mouthful, but luckily you’re always hungry for more. You’re the best adventure buddy anyone could ask for — you want to see and experience it all, and odds are you’ll have fun doing so. You have an inherent energy about you. People are drawn to your spontaneity and positive vibe. There’s no one better to see the sights and explore the world with than you."
+    },
+    {
+        name:"Capricon",
+        month:"Dec 22 to Jan 19",
+        description: "Saturn rules Capricorn, and Capricorns love rules. Saturn represents responsibility, hard work and determination. Some call it the taskmaster of the planets, keeping everyone in check and ensuring direct routes to the finish line. Saturn also has a lot to do with learning lessons and understanding limitations. Capricorns can be very self-critical — you’re hard on yourself when things don’t go according to plan and you always try to learn from your mistakes so they don’t happen again. Saturn’s influence makes Capricorns the ones you always want to have on your team and simultaneously the ones you don’t want to let down (Michelle Obama is a famous Capricorn, if that paints a better picture)."
+    },
+    {
+        name:"Aquarius",
+        month:"Jan 20 to Feb 18",
+        description:"Aquarius, you’re a breath of fresh air both literally, as the final air sign of the zodiac, and figuratively, because you’re adept at putting your signature spin on everything you touch. That’s because you’re ruled by Uranus, the planet of originality, innovation and expanded consciousness. Uranus’ energy promotes those who step outside the box and delve into new modes of thinking. For instance, Aquariuses might be the sign most open to a practice like astrology — you’re often willing to believe in things beyond what you can tangibly see. Uranus is also thought to control natural disasters. Others see that in you too, Aquarius. You’re a force to be reckoned with when it comes to your passions and interests."
+    },
+    {
+        name:"Pices",
+        month:"Feb 19  to Mar 20",
+        description:"Pisces, you’re ruled by Neptune, the God of the Seas in Roman mythology. Neptune represents dreams, the imagination and all things spiritual. You’re overflowing with creative energy and reject the hyper-realistic. You prefer to exist in the clouds, floating above the rest of the world as you toe the line between fantasy and reality. It’s existing in this dream-like state that makes you so special, Pisces. You’re in touch with a deep level of emotion and connect to others through your ability to perceive and empathize."
+    }
+    
+]
